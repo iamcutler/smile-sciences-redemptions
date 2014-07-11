7d24fb4e2ac46637531276acd922eff9e9cfc5c4
Allan Cutler <musicfan03@gmail.com>
2014-07-10 18:03:41 -0700

    Added deal redemptions config vars for logo and will enable disabled_powered before deployment

--- config/initializers/deal_redemptions.rb
+++ config/initializers/deal_redemptions.rb
@@ -1,3 +1,9 @@
 DealRedemptions.configure do |config|
   config.site_name = "Smile Sciences Redemptions"
+
+  # Set Smile Sciences logo
+  config.logo = '/assets/logo.png'
+
+  # Disable powered by credit
+  #config.disable_powered = true
 end
