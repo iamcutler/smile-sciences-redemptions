5f07090e75a8af397bab38e9d2ababa1c12913a9
Allan Cutler <musicfan03@gmail.com>
2014-08-09 08:15:12 -0700

    Overwrite default deal redemptions gem settings.

--- config/initializers/deal_redemptions.rb
+++ config/initializers/deal_redemptions.rb
@@ -1,9 +1,13 @@
 DealRedemptions.configure do |config|
+  # Set engine name
   config.site_name = "Smile Sciences Redemptions"
 
-  # Set Smile Sciences logo
+  # Set custom logo
   config.logo = '/assets/logo.png'
 
   # Disable powered by credit
-  #config.disable_powered = true
+  config.disable_powered = true
+
+  # Disable admin interface if desire to using different gui like active admin
+  # config.admin_interface = false
 end
